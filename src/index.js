63b7984a8f74c48c00a054c7137ff01e0afb1a8c
Dave Zheng <dzhen418@gmail.com>
2020-06-09 00:14:24 -0700

    node_modules

--- src/index.js
+++ src/index.js
@@ -13,17 +13,10 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true,
-    }
-  }
   renderSquare(i) {
     return <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                value={this.prop.squares[i]}
+                onClick={() => this.props.onClick(i)}
             />;
   }
 
@@ -74,6 +67,17 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        history: [{
+            squares:Array(9).fill(null)
+        }],
+        xIsNext: true;
+    }
+  }
+
+
   render() {
     return (
       <div className="game">
