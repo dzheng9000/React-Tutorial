c1526523287ea67f47434da2137a55ad8c4102b7
Dave Zheng <dzhen418@gmail.com>
2020-06-08 23:47:28 -0700

    add logic for next player

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        squares: Array(9).fill(null)
+        squares: Array(9).fill(null),
+        xIsNext: true,
     }
   }
   renderSquare(i) {
@@ -28,12 +29,15 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice(); // create a copy
-    squares[i] = 'O'
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext? 'X': 'O';
+    this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+     });
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
